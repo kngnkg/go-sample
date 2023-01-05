eff39e7d93b5d57dea716d2b8705267d8bf0c049
ryokawa <kawata.ryo.0518@gmail.com>
2023-01-05 10:12:39 +0000

    graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kwtryo/go-sample/config"
@@ -17,14 +22,6 @@ func main() {
 	}
 }
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	return r
-}
-
 func run() error {
 	cfg, err := config.New()
 	if err != nil {
@@ -33,8 +30,43 @@ func run() error {
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		// graceful shutdownの確認
+		time.Sleep(5 * time.Second)
+		c.String(http.StatusOK, "pong")
 	})
 
-	return r.Run(fmt.Sprintf(":%d", cfg.Port))
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Port),
+		Handler: r,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+
+	return err
 }
--- main_test.go
+++ main_test.go
@@ -1,27 +1,8 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
-
-func TestPingRoute(t *testing.T) {
-	t.Skip("リファクタリング中")
-	router := setupRouter()
-
-	got := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/ping", nil)
-	if err != nil {
-		t.Fatalf("cannot create request: %v", err)
-	}
-	router.ServeHTTP(got, req)
-
-	assert.Equal(t, 200, got.Code)
-	assert.Equal(t, "pong", got.Body.String())
-}
 
 func TestRun(t *testing.T) {
 	t.Skip("リファクタリング中")
