465255657419793d74536940c118a280869d2e06
ryokawa <kawata.ryo.0518@gmail.com>
2023-01-10 11:08:32 +0000

    add interface and clock

--- store/repository.go
+++ store/repository.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 
+	"github.com/kwtryo/go-sample/clock"
 	"github.com/kwtryo/go-sample/config"
 )
 
@@ -24,10 +25,44 @@ func New(cfg *config.Config) (*sqlx.DB, func(), error) {
 		return nil, nil, err
 	}
 
-	// Openは接続確認が行われないため、ここで確認する。
+	// sql.Openは接続確認が行われないため、ここで確認する。
 	if err := db.Ping(); err != nil {
 		return nil, func() { _ = db.Close() }, err
 	}
 	xdb := sqlx.NewDb(db, driver)
 	return xdb, func() { _ = db.Close() }, nil
+}
+
+type Beginner interface {
+	BeginTx(opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	Preparex(query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	Exec(query string, args ...any) (sql.Result, error)
+	NamedExec(query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	Queryx(query string, args ...any) (*sqlx.Rows, error)
+	QueryRow(query string, args ...any) *sqlx.Row
+	Get(dest interface{}, query string, args ...any) error
+	Select(dest interface{}, query string, args ...any) error
+}
+
+var (
+	// インタフェースが期待通りに宣言されているか確認
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sql.Tx)(nil)
+)
+
+type Repository struct {
+	Clocker clock.Clocker
 }
