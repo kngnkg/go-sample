c8f07006d2fe111abf535d6226866d214ab80905
ryokawa <kawata.ryo.0518@gmail.com>
2023-01-22 23:22:44 +0000

    add transaction middleware

--- router/router.go
+++ router/router.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kwtryo/go-sample/clock"
 	"github.com/kwtryo/go-sample/config"
 	"github.com/kwtryo/go-sample/handler"
+	"github.com/kwtryo/go-sample/middleware"
 	"github.com/kwtryo/go-sample/service"
 	"github.com/kwtryo/go-sample/store"
 )
@@ -23,13 +24,13 @@ func SetupRouter(cfg *config.Config) (*gin.Engine, func(), error) {
 	}
 
 	router := gin.Default()
-	// router.Use(middleware.DBTransactionMiddleware(db))
+	router.Use(middleware.DBTransactionMiddleware(db))
 
 	// ヘルスチェック
 	router.GET("/health", func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
 	router.POST("/register", userHandler.RegisterUser)
